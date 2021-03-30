318cb7ceef957d37ce760d489f330537b1e4933e
Rakshith658 <rakshithkumars7777@gmail.com>
2021-03-30 14:22:00 +0530

    auth is added with the help of amplify

--- App.js
+++ App.js
@@ -2,15 +2,22 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import MyTabs from './Navigation/bottomTab';
 import NewTweetStackScreen from './Screens/NewTweetScreen';
+
+import { withAuthenticator } from 'aws-amplify-react-native'
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+Amplify.configure(config)
 
 
 const Stack = createStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer >
       <Stack.Navigator screenOptions={{headerShown:false}}>
@@ -20,6 +27,8 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+export default withAuthenticator(App)
 
 const styles = StyleSheet.create({
   container: {
